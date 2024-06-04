714573ced9c67b80c36a98dc8bb0dcd26143f8cd
felipema <felipema90@gmail.com>
2024-06-04 00:43:10 +0000

    modifica frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Pense no que faz teu coração acelerar. O que primeiro te vem a mente?</h1>
+    return <body><h1>Pense no que faz teu coração acelerar. </h1><h2>O que primeiro te vem a mente?</h2></body>
 }
 
 export default Home;
